c89c77f3e4a411022d064e0af040022f5d2148d6
Avikambir10 <123386663+Avikambir10@users.noreply.github.com>
2023-11-01 19:47:46 +0530

    tower of hanoi using recursion

---
+++ TOWER OF HANOI.cpp
@@ -0,0 +1,21 @@
+// TOWER OF HANOI>>>>
+#include <iostream>
+using namespace std;
+void towerofHANOI(int n, char source, char helper, char destinaton)
+{
+    if (n == 0){
+       return;
+    }
+    towerofHANOI(n - 1, source, destinaton, helper);
+    cout << "Move from " << source << " to " << destinaton << "\n";
+    towerofHANOI(n - 1, helper, source, destinaton);
+
+}
+int main()
+{
+    int n;
+    cin >> n;
+    towerofHANOI(n, 'A', 'B', 'C');
+    cout <<  "\nNumber of Moves =" << (n*n)-1 ;
+    return 0;
+}
